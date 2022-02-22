6f364b8f92b3dece06e920c1f5286bcfcc272fef
Vlad <vlad.mikhnenok.95@mail.ru>
2022-02-22 18:20:14 +0300

    Добавлены правила игры

--- app/src/main/java/com/vladmikh/projects/four_by_four/GameRulesActivity.java
+++ app/src/main/java/com/vladmikh/projects/four_by_four/GameRulesActivity.java
@@ -2,13 +2,19 @@ package com.vladmikh.projects.four_by_four;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameRulesActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_rules);
+
+
     }
 }
