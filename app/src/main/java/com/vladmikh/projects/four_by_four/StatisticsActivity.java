e7eec650a3e5005f9ff55db860694b093038eaab
Vlad <vlad.mikhnenok.95@mail.ru>
2022-04-14 21:32:03 +0300

    Переделана StatisticsActivity

--- app/src/main/java/com/vladmikh/projects/four_by_four/StatisticsActivity.java
+++ app/src/main/java/com/vladmikh/projects/four_by_four/StatisticsActivity.java
@@ -8,12 +8,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class StatisticsActivity extends AppCompatActivity {
+public class StatisticsActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener, CustomSpinner.OnSpinnerEventsListener{
 
     private TextView textViewNoTimeVictory;
     private TextView textViewNoTimeGame;
@@ -29,7 +33,7 @@ public class StatisticsActivity extends AppCompatActivity {
     private TextView textViewFiveMinPercent;
 
     private SharedPreferences sharedPreferences;
-    private Switch switchGameMode;
+    private CustomSpinner spinner;
 
 
     @Override
@@ -65,20 +69,16 @@ public class StatisticsActivity extends AppCompatActivity {
         textViewFiveMinVictory = findViewById(R.id.textViewFiveMinVictory);
         textViewFiveMinGame = findViewById(R.id.textViewFiveMinGame);
         textViewFiveMinPercent = findViewById(R.id.textViewFiveMinPercent);
-        switchGameMode = findViewById(R.id.switchStatistics);
+        spinner = findViewById(R.id.spinnerGameRules);
+        spinner.setSpinnerEventsListener(this);
+
+        ArrayAdapter adapterGame = ArrayAdapter.createFromResource(this, R.array.gameSettings, R.layout.spinner_choosen_item);
+        adapterGame.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner.setAdapter(adapterGame);
+        spinner.setOnItemSelectedListener(this);
 
         setResults();
 
-        switchGameMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b) {
-                    setResultsPartition();
-                } else {
-                    setResults();
-                }
-            }
-        });
 
     }
 
@@ -160,13 +160,64 @@ public class StatisticsActivity extends AppCompatActivity {
         textViewFiveMinPercent.setText(percent);
     }
 
-    public void onClickSimple(View view) {
-        setResults();
-        switchGameMode.setChecked(false);
+    private void setResultsMobile() {
+        int victory;
+        int game;
+        String percent;
+
+        victory = sharedPreferences.getInt(MainActivity.NO_TIME_LIMIT_VICTORY_MOBILE,0);
+        game = sharedPreferences.getInt(MainActivity.NO_TIME_LIMIT_GAME_MOBILE, 0);
+        percent = calculatePercent(victory, game) + " %";
+        textViewNoTimeVictory.setText(String.valueOf(victory));
+        textViewNoTimeGame.setText(String.valueOf(game));
+        textViewNoTimePercent.setText(percent);
+
+        victory = sharedPreferences.getInt(MainActivity.ONE_MIN_VICTORY_MOBILE, 0);
+        game = sharedPreferences.getInt(MainActivity.ONE_MIN_GAME_MOBILE, 0);
+        percent = calculatePercent(victory, game) + " %";
+        textViewOneMinVictory.setText(String.valueOf(victory));
+        textViewOneMinGame.setText(String.valueOf(game));
+        textViewOneMinPercent.setText(percent);
+
+        victory = sharedPreferences.getInt(MainActivity.THREE_MIN_VICTORY_MOBILE, 0);
+        game = sharedPreferences.getInt(MainActivity.THREE_MIN_GAME_MOBILE, 0);
+        percent = calculatePercent(victory, game) + " %";
+        textViewThreeMinVictory.setText(String.valueOf(victory));
+        textViewThreeMinGame.setText(String.valueOf(game));
+        textViewThreeMinPercent.setText(percent);
+
+        victory = sharedPreferences.getInt(MainActivity.FIVE_MIN_VICTORY_MOBILE, 0);
+        game = sharedPreferences.getInt(MainActivity.FIVE_MIN_GAME_MOBILE, 0);
+        percent = calculatePercent(victory, game) + " %";
+        textViewFiveMinVictory.setText(String.valueOf(victory));
+        textViewFiveMinGame.setText(String.valueOf(game));
+        textViewFiveMinPercent.setText(percent);
     }
 
-    public void onClickPartition(View view) {
-        setResultsPartition();
-        switchGameMode.setChecked(true);
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if(i == 0) {
+            setResults();
+        } else if (i == 1) {
+            setResultsPartition();
+        } else {
+            setResultsMobile();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onPopupWindowOpened(Spinner spinner) {
+        spinner.setBackground(getResources().getDrawable(R.drawable.spinner_time_up_style));
+    }
+
+    @Override
+    public void onPopupWindowClosed(Spinner spinner) {
+        spinner.setBackground(getResources().getDrawable(R.drawable.spinner_time_style));
     }
 }
